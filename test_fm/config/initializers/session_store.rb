f028219ce7c0cda0b55f7149ceecdf8c429bc650
Andrey Gorelov <ekonomizer@mail.ru>
2013-06-18 13:59:30 +0400

    upgrade by new rails 4 version 4.0.0.rc2

--- test_fm/config/initializers/session_store.rb
+++ test_fm/config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TestFm::Application.config.session_store :encrypted_cookie_store, key: '_test_fm_session'
+TestFm::Application.config.session_store :cookie_store, key: '_test_fm_session'
