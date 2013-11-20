8d082fa41628d427f5ce4e834e8327abff1979ab
Andrey Gorelov <ekonomizer@mail.ru>
2013-11-20 17:31:40 +0400

    fix

--- config/initializers/init_cached_items.rb
+++ config/initializers/init_cached_items.rb
@@ -1,10 +1,15 @@
 p 'INIT START CACHE'
-Country.cached_countries
-Universe.cached_filled_universe
-League.cached_count
-Club.cached_clubs
 
-League.all.each do |league|
-  Country.countries_by_league_id(league.id)
+def init
+  Country.cached_countries
+  Universe.cached_filled_universe
+  League.cached_count
+  Club.cached_clubs
+
+  League.all.each do |league|
+    Country.countries_by_league_id(league.id)
+  end
 end
+
+#init
 p 'CACHE INITIALIZED'
